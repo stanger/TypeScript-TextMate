72369994dd575566adc22f205a738b1680e45e15
Troy Stanger <stanger@zumero.com>
2017-02-22 13:24:06 -0600

    Initial checkin on the tmbundle

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TypeScript-TextMate
-A TextMate bundle for the TypeScript lanugage
+A TextMate bundle for the TypeScript language.
+
+This repo contains a simple script for fetching the latest from [Microsoft/TypeScript-TmLanguage](https://github.com/Microsoft/TypeScript-TmLanguage) and building a new tmBundle.
+
+Download the bundle here: [Latest](https://github.com/stanger/TypeScript-TextMate/blob/master/dist/TypeScripts.tmbundle.zip) 
+
---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+OUTPUT=./dist
+BUNDLE_PATH="$OUTPUT/TypeScript.tmbundle"
+
+if [ -e $BUNDLE_PATH ] ; then
+	rm -r $BUNDLE_PATH
+fi
+
+pushd TypeScript-TmLanguage
+
+git pull
+#TODO should a build be run here?
+
+popd
+
+mkdir -p $BUNDLE_PATH/Preferences
+mkdir -p $BUNDLE_PATH/Syntaxes
+
+cp TypeScript-TmLanguage/*.tmPreferences $BUNDLE_PATH/Preferences
+cp TypeScript-TmLanguage/*.tmLanguage $BUNDLE_PATH/Syntaxes
+cp src/* $BUNDLE_PATH
+
+pushd $OUTPUT
+
+zip TypeScript.tmbundle.zip TypeScript.tmbundle
+
+popd
