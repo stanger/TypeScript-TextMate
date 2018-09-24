3c6ba7fa6d930c5b61fb0c70f35c5d455bb0c81f
Troy Stanger <stanger@zumero.com>
2018-09-24 10:07:25 -0500

    Created new bundle from upstream changes

--- build.sh
+++ build.sh
@@ -17,8 +17,8 @@ popd
 mkdir -p $BUNDLE_PATH/Preferences
 mkdir -p $BUNDLE_PATH/Syntaxes
 
-cp TypeScript-TmLanguage/*.tmPreferences $BUNDLE_PATH/Preferences
-cp TypeScript-TmLanguage/*.tmLanguage $BUNDLE_PATH/Syntaxes
+cp TypeScript-TmLanguage/*tmPreferences $BUNDLE_PATH/Preferences
+cp TypeScript-TmLanguage/*tmLanguage $BUNDLE_PATH/Syntaxes
 cp src/* $BUNDLE_PATH
 
 pushd $OUTPUT
